568d9f2ef8c940ce150df339d5a5aa281835acfa
Joe.C.LEUNG <joe.c.leung@accenture.com>
2021-05-05 19:20:41 +0800

    @ViewChild

--- src/app/components/emily/emily.component.ts
+++ src/app/components/emily/emily.component.ts
@@ -1,18 +1,21 @@
-import {Component, OnInit} from '@angular/core';
-import {Input, Output} from '@angular/core';
-import {EventEmitter} from '@angular/core';
+import {Component, OnInit, OnChanges, SimpleChanges} from '@angular/core';
+import {Input, Output, EventEmitter} from '@angular/core';
+import {ViewChild, ElementRef} from '@angular/core';
 
 @Component({
     selector: 'app-emily',
     templateUrl: './emily.component.html',
     styleUrls: ['./emily.component.scss']
 })
-export class EmilyComponent implements OnInit {
+export class EmilyComponent implements OnInit, OnChanges {
+
+    @ViewChild('lastUpdate', { static: true }) lastUpdate?: ElementRef;
 
     default = 12;
+    smallClass = [''];
     @Input() color = '';
     @Input() size: number | string = 0;
-    @Output() sizeChange = new EventEmitter<number>();
+    @Output() sizeChange = new EventEmitter<number | string>();
 
     constructor() {
         this.reset(this.default);
@@ -21,9 +24,13 @@ export class EmilyComponent implements OnInit {
     ngOnInit(): void {
     }
 
+    ngOnChanges(changes: SimpleChanges): void {
+        this.update('ngOnChanges');
+    }
+
     reset(value: number): void {
         this.size = value;
-        this.sizeChange.emit(this.size);
+        this.update('reset');
     }
 
     reduce(): void {
@@ -36,7 +43,20 @@ export class EmilyComponent implements OnInit {
 
     resize(delta: number): void {
         this.size = Math.min(40, Math.max(8, +this.size + delta));
+        this.update('resize');
+    }
+
+    update(from: string): void {
+        const date = new Date().toLocaleString();
         this.sizeChange.emit(this.size);
+        if (this.lastUpdate) {
+            this.smallClass.push('animation-play');
+            this.lastUpdate.nativeElement.innerHTML = `${date}, from <strong>${from}</strong>`;
+            setTimeout(() => {
+                delete this.smallClass[this.smallClass.indexOf('animation-play')];
+                this.smallClass = Array.from(new Set(this.smallClass));
+            }, 1200);
+        }
     }
 
 }
